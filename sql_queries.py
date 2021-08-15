ecc90888aa64002b423359cc786aa9bd134768a8
Rolando Gonzalez <gor8mx@bosch.com>
2021-08-15 03:19:05 -0500

    refactor: Change lists order

--- sql_queries.py
+++ sql_queries.py
@@ -120,8 +120,9 @@ songplay_table_create = (
     ");\n"
 )
 
-# INSERT RECORDS
+print(time_table_create)
 
+# INSERT RECORDS
 songplay_table_insert = ("""
 """)
 
@@ -139,11 +140,22 @@ time_table_insert = ("""
 """)
 
 # FIND SONGS
-
 song_select = ("""
 """)
 
 # QUERY LISTS
+create_table_queries = [
+    time_table_create,
+    user_table_create,
+    artist_table_create,
+    song_table_create,
+    songplay_table_create
+]
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+drop_table_queries = [
+    songplay_table_drop,
+    time_table_drop,
+    user_table_drop,
+    song_table_drop,
+    artist_table_drop
+]

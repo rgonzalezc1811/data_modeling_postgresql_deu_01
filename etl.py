09140b0b60acd47fdd46d2b4d3cd2f19e30c18ae
Rolando Gonzalez <gor8mx@bosch.com>
2021-08-17 14:25:24 -0500

    feat: Upload songs and artists from song files

--- etl.py
+++ etl.py
@@ -1,53 +1,86 @@
-import os
+# -*- coding: utf-8 -*-
+"""Script to perform ETL process.
+
+Mantaniner: Rolando Gonzalez
+Version: 1.0.0
+"""
+# Standard library imports
 import glob
+import os
+import re
+import sys
+
+# Third-party imports
 import psycopg2
 import pandas as pd
-from sql_queries import *
 
+# Propietary imports
+import sql_queries as sq
+
+def single_quote_converter(sentence):
+    sentence = re.sub(r'\'', r'<single_quote_tag>', sentence)
+
+    return sentence
 
 def process_song_file(cur, filepath):
     # open song file
-    df = 
-
-    # insert song record
-    song_data = 
-    cur.execute(song_table_insert, song_data)
+    df = pd.read_json(filepath, lines=True)
     
     # insert artist record
-    artist_data = 
-    cur.execute(artist_table_insert, artist_data)
+    columns = [
+        "artist_id",
+        "artist_name",
+        "artist_location",
+        "artist_latitude",
+        "artist_longitude"
+    ]
+    artist_data = df[columns].copy()
+    artist_data[:]["artist_name"] = artist_data["artist_name"].map(
+        single_quote_converter, na_action='ignore'
+    )
+    artist_data[:]["artist_location"] = artist_data["artist_location"].map(
+        single_quote_converter, na_action='ignore'
+    )
+    cur.execute(sq.artist_table_insert(dataframe=artist_data))
 
+    # insert song record
+    columns = ["song_id", "title", "artist_id", "year", "duration"]
+    song_data = df[columns]
+    song_data[:]["title"] = song_data["title"].map(
+            single_quote_converter, na_action='ignore'
+    )
+    cur.execute(sq.song_table_insert(dataframe=song_data))
 
 def process_log_file(cur, filepath):
     # open log file
-    df = 
+    df = None
 
     # filter by NextSong action
-    df = 
+    df = None
 
     # convert timestamp column to datetime
-    t = 
+    t = None
     
     # insert time data records
-    time_data = 
-    column_labels = 
-    time_df = 
+    time_data = None
+    column_labels = None
+    time_df = None
 
     for i, row in time_df.iterrows():
-        cur.execute(time_table_insert, list(row))
+        cur.execute(sq.time_table_insert, list(row))
 
     # load user table
-    user_df = 
+    user_df = None
 
     # insert user records
     for i, row in user_df.iterrows():
-        cur.execute(user_table_insert, row)
+        cur.execute(sq.user_table_insert, row)
 
     # insert songplay records
     for index, row in df.iterrows():
         
         # get songid and artistid from song and artist tables
-        cur.execute(song_select, (row.song, row.artist, row.length))
+        cur.execute(sq.song_select, (row.song, row.artist, row.length))
         results = cur.fetchone()
         
         if results:
@@ -56,12 +89,12 @@ def process_log_file(cur, filepath):
             songid, artistid = None, None
 
         # insert songplay record
-        songplay_data = 
-        cur.execute(songplay_table_insert, songplay_data)
+        songplay_data = None
+        cur.execute(sq.songplay_table_insert, songplay_data)
 
 
 def process_data(cur, conn, filepath, func):
-    # get all files matching extension from directory
+    # Get all files matching extension from directory
     all_files = []
     for root, dirs, files in os.walk(filepath):
         files = glob.glob(os.path.join(root,'*.json'))
@@ -80,10 +113,18 @@ def process_data(cur, conn, filepath, func):
 
 
 def main():
-    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    conn = psycopg2.connect(
+        user="admin",
+        password="98573Hgte", # <password>
+        host="localhost",
+        port="5432",
+        dbname="sparkifydb"
+    )
     cur = conn.cursor()
 
     process_data(cur, conn, filepath='data/song_data', func=process_song_file)
+    conn.close()
+    sys.exit(0)
     process_data(cur, conn, filepath='data/log_data', func=process_log_file)
 
     conn.close()

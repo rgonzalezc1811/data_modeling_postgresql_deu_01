75619f680f9e4ee518c849388fbab4fd72b06995
Rolando Gonzalez <gor8mx@bosch.com>
2021-08-19 00:53:54 -0500

    docstrings: Complete the comments

--- create_tables.py
+++ create_tables.py
@@ -14,12 +14,14 @@ import traceback
 import psycopg2
 
 # Propietary imports
-from sql_queries import create_table_queries, drop_table_queries
+import sql_queries as sq
 
 def create_database():
-    """Creates and connects to the 'sparkifydb' database
+    """Creates and connects to the 'data_modeling_postgres_01' database
 
-    This database represents the Sparkify database to handle JSON data.
+    This database is used to create the 'sparkifydb', then disconnects
+    from 'data_modeling_postgres_01' and reconnects to the created
+    database 'sparkifydb'.
 
     Returns
     -------
@@ -33,22 +35,23 @@ def create_database():
         format -> No apply
         options -> No apply
     """
-    # Connect to the database
+    # Connect to the database, pay attention to the credentials!
+    # TODO: Verify all the fields and insert password.
     try:
         conn = psycopg2.connect(
             user="admin",
             password="<password>",
-            host="localhost", 
+            host="localhost",
             port="5432",
             dbname="data_modeling_postgres_01"
         )
         conn.set_session(autocommit=True)
         cur = conn.cursor()
 
-    except psycopg2.Error as e:
+    except psycopg2.Error as error:
         message = traceback.format_exc()
         print("ERROR: Connection to the PostgreSQL database failed!!! :c\n")
-        print(f"Error:\n{e}\n")
+        print(f"Error:\n{error}\n")
         print(f"Complete log error:\n{message}\n")
 
     # Create sparkify database with UTF8 encoding
@@ -59,30 +62,32 @@ def create_database():
 
     # close connection to default database
     conn.close()
-    
-    # Connect to 'sparkifydb' database
+
+    # Connect to 'sparkifydb' database, becareful with the credentials!
     try:
+        # TODO: Verify all the fields and insert password.
         conn = psycopg2.connect(
             user="admin",
-            password="<password>", # <password>
-            host="localhost", 
+            password="<password>",
+            host="localhost",
             port="5432",
             dbname="sparkifydb"
         )
         cur = conn.cursor()
 
-    except psycopg2.Error as e:
+    except psycopg2.Error as error:
         message = traceback.format_exc()
         print("ERROR: Connection to the PostgreSQL database failed!!! :c\n")
-        print(f"Error:\n{e}\n")
+        print(f"Error:\n{error}\n")
         print(f"Complete log error:\n{message}\n")
 
     return cur, conn
 
 def drop_tables(cur, conn):
-    """Drop all the crated tables.
-    
-    The function uses the queries in 'drop_table_queries' list.
+    """Drop all the crated tables if they exist
+
+    The function uses the queries in 'drop_table_queries' list, the list
+    is located in the 'sql_queries.py' file.
 
     Parameters
     ----------
@@ -96,16 +101,17 @@ def drop_tables(cur, conn):
         format -> No apply
         options -> No apply
     """
-    for query in drop_table_queries:
+    for query in sq.drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
-    """ Creat the tables.
+    """ Creat the tables if they no exist.
 
-    Each table is created using the queries in `create_table_queries`
-    list.
+    Each table is created using the queries in the
+    `create_table_queries` list, the list is located in the
+    'sql_queries.py' file.
 
     Parameters
     ----------
@@ -119,19 +125,19 @@ def create_tables(cur, conn):
         format -> No apply
         options -> No apply
     """
-    for query in create_table_queries:
+    for query in sq.create_table_queries:
         cur.execute(query)
         conn.commit()
 
 def main():
     """ Pipeline execution.
 
-    - Drops (if exists) and Creates the sparkify database. 
-    - Establishes connection with the sparkify database and gets
+    * Drop (if exists) and Create the sparkify database.
+    * Establish connection with the sparkify database and get
       cursor to it.
-    - Drops all the tables.
-    - Creates all tables needed.
-    - Finally, closes the connection.
+    * Drop all the tables if they exist.
+    * Create all the tables needed if they no exist.
+    * Close the connection.
     """
     cur, conn = create_database()
     drop_tables(cur, conn)
